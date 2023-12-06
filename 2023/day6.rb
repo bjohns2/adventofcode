517695d3633d0544f74fb5071327f11a6cfaa195
Brittney Johnson <brittneyejohnson@gmail.com>
2023-12-06 09:57:18 -0500

    solved day 6

---
+++ 2023/day6.rb
@@ -0,0 +1,19 @@
+input = "Time:        35     69     68     87
+Distance:   213   1168   1086   1248"
+
+input_formatted = [[35,213], [69,1169], [68,1086], [87,1248]]
+
+all_winning_ways = 1
+input_formatted.each do |race|
+  time = race[0]
+  distance = race[1]
+
+  winning_ways = 0
+
+  (1..time).each do |acceleration_time|
+    winning_ways += 1 if acceleration_time * (time-acceleration_time) > distance
+  end
+  puts "race #{race}: can win #{winning_ways} different ways"
+  all_winning_ways *= winning_ways
+end
+puts all_winning_ways
