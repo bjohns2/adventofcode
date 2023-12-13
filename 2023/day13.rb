859d1fb96e15e4d3f14ba286c16fbae87a1e83e6
Brittney Johnson <brittneyejohnson@gmail.com>
2023-12-13 11:18:10 -0500

    day 13 pt 2

--- 2023/day13.rb
+++ 2023/day13.rb
@@ -1372,10 +1372,14 @@ def format_input(input)
   end
 end
 
-def solve_puzzle(puzzle)
+def solve_puzzle(puzzle_in)
+  puzzle = puzzle_in.map{|line| line.map.with_index{|e,i| e == '.' ? '.' : i} }
+  # puts puzzle.map{|line| line.join('')}.join("\n")
+
   (0..puzzle.size).each do |line_index|
-    # puts "line index #{line_index}"
-    all_matches = true
+    # puts "line index #{line_index} for puzzle size #{puzzle.size}"
+    non_matches = []
+    off_by_one_matches = []
     (0..line_index).each do |line_index_offset|
       # puts "checking line_index_offset #{line_index_offset} so puzzle[#{line_index-line_index_offset}] and puzzle[#{line_index+1+line_index_offset}]"
       first_line = line_index-line_index_offset
@@ -1383,10 +1387,18 @@ def solve_puzzle(puzzle)
       if first_line < 0 || second_line >= puzzle.size
         break
       elsif puzzle[first_line] != puzzle[second_line]
-        all_matches = false
+        non_matches << line_index_offset
+        first_compare = (puzzle[first_line].uniq - puzzle[second_line].uniq).size
+        second_compare = (puzzle[second_line].uniq - puzzle[first_line].uniq).size
+        # puts "#{puzzle[first_line]} - #{puzzle[second_line]} size is #{(puzzle[first_line] - puzzle[second_line]).size}"
+        # puts "#{puzzle[second_line]} - #{puzzle[first_line]} size is #{(puzzle[second_line] - puzzle[first_line]).size}"
+        off_by_one_matches << line_index_offset if (first_compare == 1 && second_compare == 0) || (first_compare == 0 && second_compare == 1)
       end
     end
-    return line_index+1 if all_matches && !(line_index+1 >= puzzle.size)
+    # return line_index+1 if non_matches == [] && !(line_index+1 >= puzzle.size)
+    # puts "#{non_matches.size} non_matches, #{off_by_one_matches.size} off by ones"
+
+    return line_index+1 if off_by_one_matches.size == 1 &&  non_matches.size == 1 && !(line_index+1 >= puzzle.size)
   end
   0
 end
@@ -1394,27 +1406,17 @@ end
 def solve(input)
   puzzles = format_input(input)
 
-  # puzzles.each do |puzzle|
-  #   puts puzzle.map{|line| line.join('')}.join("\n")
-  #   puts "\n"
-  #   puts puzzle.transpose.map{|line| line.join('')}.join("\n")
-  #   puts "\n"
-
-  #   puts "\n"
-
-  # end
-
   row_sum = puzzles.sum do |puzzle|
-    solution = solve_puzzle(puzzle)
-    solution
-  end
+    row_answer = solve_puzzle(puzzle)
+    col_answer = solve_puzzle(puzzle.transpose)
 
-  col_sum = puzzles.sum do |puzzle|
-    solution = solve_puzzle(puzzle.transpose)
-    solution
+    puts "row_answer: #{row_answer}, col_answer: #{col_answer}"
+    puts puzzle.map{|line| line.join('')}.join("\n") if row_answer == col_answer && col_answer == 0
+    col_answer + (row_answer * 100)
   end
-
-  col_sum + row_sum * 100
 end
 
 puts solve(my_input)
+
+# 27519 is too low
+# 36025 is too low
