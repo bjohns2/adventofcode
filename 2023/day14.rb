83530a1225f26a5454f93e2c2f16363cf1869368
Brittney Johnson <brittneyejohnson@gmail.com>
2023-12-14 13:04:46 -0500

    d14 p2

--- 2023/day14.rb
+++ 2023/day14.rb
@@ -125,7 +125,7 @@ def shift_rocks(map)
     new_sections = line_sections.map do |line_section|
       rock_count = line_section.count('O')
       empty_count = line_section.count('.')
-      '0' * rock_count + '.' * empty_count
+      'O' * rock_count + '.' * empty_count
     end
     new_sections.join('#').split('')
   end
@@ -136,7 +136,7 @@ def count_load(map)
   map.each do |line|
     line_size = line.size
     line.each_with_index do |char, i|
-      total_load += (line_size - i) if char == '0'
+      total_load += (line_size - i) if char == 'O'
     end
   end
   total_load
@@ -144,21 +144,95 @@ end
 
 def print_map(message, map)
   printed_map = map.map{|line| line.join('')}.join("\n")
-  puts "#{message}: \n#{printed_map}"
+  puts "#{message}: \n#{printed_map}\n\n"
+end
+
+def rotate_clockwise(map)
+  map.transpose.map(&:reverse)
 end
 
 def solution(input)
   map = format_input(input)
-  # print_map('map',map)
+  # print_map("original map", map)
 
-  transposed_map = map.transpose
-  # print_map('transposed map',transposed_map)
+  # point north to the left
+  north_right = rotate_clockwise(map)
+  north_down = rotate_clockwise(north_right)
+  north_left = rotate_clockwise(north_down)
+  # shifted_map = shift_rocks(transposed_map)
 
-  shifted_map =  shift_rocks(transposed_map)
-  # print_map('shifted map',shifted_map)
+  # # shift west
+  # transposed_map = map.transpose
+  # shifted_map = shift_rocks(transposed_map)
 
-  total_load = count_load(shifted_map)
-  puts total_load
+  memoized_shifts = {}
+  size_locations = {}
+
+  (1..10001).each do |i|
+    start_string = north_left.map{|line| line.join('')}.join("\n")
+
+    mem_check = memoized_shifts[start_string]
+    if mem_check
+      north_left = mem_check[0]
+      size_locations[mem_check[1]] << i
+    else
+      #shift north
+      shifted_map_north_left = shift_rocks(north_left)
+      # print_map("shifted north, north left", shifted_map_north_left)
+
+      north_up = rotate_clockwise(shifted_map_north_left)
+
+      # west
+      shifted_map_north_up = shift_rocks(north_up)
+      # print_map("shifted west, north up", shifted_map_north_up)
+
+      north_right = rotate_clockwise(shifted_map_north_up)
+
+      # shift south
+      shifted_map_north_right = shift_rocks(north_right)
+      # print_map("shifted south, north right", shifted_map_north_right)
+
+      north_down = rotate_clockwise(shifted_map_north_right)
+
+      # shift east
+      shifted_map_north_down = shift_rocks(north_down)
+      # print_map("shifted east, north down", shifted_map_north_down)
+
+      north_left = rotate_clockwise(shifted_map_north_down)
+      north_up = rotate_clockwise(north_left)
+
+      # end_string = north_left.map{|line| line.join('')}.join("\n")
+      counted_load = count_load(north_left)
+      memoized_shifts[start_string] = [north_left, counted_load, i]
+      size_locations[counted_load] = [i]
+    end
+    
+    
+
+    # print_map("north up", north_up) if i % 100000 == 0
+
+    total_load = count_load(north_left)
+
+    puts "did shift #{i}, mem size #{memoized_shifts.size}, transposed so north is to the left, load #{total_load}" if i % 1000 == 0 #|| total_load > 0
+    puts "memed sizes: #{memoized_shifts.values().map{|v| "load #{v[1]} on i#{v[2]}"}.join(',')}" if i % 1000 == 0 #|| total_load > 0
+    if i % 10000 == 0 #|| total_load > 0
+      puts "size_locations:" 
+      size_locations.each do |k,v|
+        puts "size #{k} appears in locations:\n#{v.join(',')}\n\n"
+      end
+    end
+
+    # transposed_map = transposed_map.transpose.map{|l| l.reverse}
+    # puts "on shift #{i}, load #{count_load(shifted_map)}" if i % 100 == 0
+
+  end
+
+  # total_load = count_load(shifted_map)
+  # puts total_load
 end
 
 solution(my_input)
+
+# 93917 is too high
+
+# cheated a bit; looked at output and saw cycle length was 51. 1000000000-(51*19607700) =7300 and total weight for 7300 was 93742
