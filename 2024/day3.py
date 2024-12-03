ff636a1ce3241dbabaf976f01108150ad28339ee
Brittney Johnson <brittneyejohnson@gmail.com>
2024-12-03 10:25:17 -0500

    d3p2

--- 2024/day3.py
+++ 2024/day3.py
@@ -5,8 +5,8 @@ test_input = open("./2024/test_input.txt", "r").read()
 input = my_input
 
 
-pattern = r'mul\(\d{1,3},\d{1,3}\)'
-
+# pattern = r'mul\(\d{1,3},\d{1,3}\)'
+pattern2 = r'(mul\(\d{1,3},\d{1,3}\))|(don\'t\(\))|(do\(\))'
 
 def parse_match(match):
   first_num = match.split('(')[1].split(',')[0]
@@ -14,8 +14,18 @@ def parse_match(match):
   return [int(first_num), int(second_num)]
 
 total = 0
-matches = re.findall(pattern, input) 
+matches = re.findall(pattern2, input) 
+# print(matches)
+enabled = True
 for match in matches:
-  parsed_match = parse_match(match)
-  total += parsed_match[0] * parsed_match[1]
+  print(match)
+  if match[2] != '':
+    print('enabling')
+    enabled = True
+  elif match[1] != "":
+    print('disabling')
+    enabled = False
+  elif enabled:
+    parsed_match = parse_match(match[0])
+    total += parsed_match[0] * parsed_match[1]
 print(total)
