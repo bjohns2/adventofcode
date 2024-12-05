2601f5b4631a48bc8e7c10f23c031ea84b2b7b54
Brittney Johnson <brittneyejohnson@gmail.com>
2024-12-05 10:35:22 -0500

    d5p2

--- 2024/day5.py
+++ 2024/day5.py
@@ -1,7 +1,8 @@
 import math
+import functools
 my_input = open("./2024/my_input.txt", "r").read()
 test_input = open("./2024/test_input.txt", "r").read()
-input = my_input
+input = test_input
 
 order_str = input.split("\n\n")[0]
 update_str = input.split("\n\n")[1]
@@ -17,13 +18,32 @@ def is_ordered(update):
   return True
 
 ordered_updates = []
+unordered_updates = []
 for update_str in updates:
   update = update_str.split(',')
   if is_ordered(update):
     ordered_updates.append(update)
+  else:
+    unordered_updates.append(update)
+
+
+def cmp_updates(a, b):
+    if f"{a}|{b}" in orders:
+        return 1
+    elif f"{b}|{a}" in orders:
+        return -1
+    else:
+        return 0
+
+def order_update(unordered_update):
+  return sorted(unordered_update, key=functools.cmp_to_key(cmp_updates))
+
+reordered_updates = []
+for unordered_update in unordered_updates:
+  reordered_updates.append(order_update(unordered_update))
 
 total = 0
-for update in ordered_updates:
+for update in reordered_updates:
   middle_num = update[math.floor(len(update)/2)]
   total += int(middle_num)
 print(total)
