2c8b17ef3628ce63d13b47c74bce1a22e11f0ae4
Brittney Johnson <brittneyejohnson@gmail.com>
2024-12-07 00:55:44 -0500

    d7p2

--- 2024/day7.py
+++ 2024/day7.py
@@ -9,20 +9,43 @@ eqs = input.split("\n")
 def is_solvable(value, inputs):
   if int(value) != value:
       return False
+  value = int(value)
   if len(inputs) == 1:
     if inputs[0] == value:
-     return True
+      print('value',value, 'inputs', inputs)
+      return True
     else:
       return False
-  return is_solvable(value-inputs[-1], inputs[:-1]) or is_solvable(value/inputs[-1], inputs[:-1])
+  print('value', value, "inputs", inputs)
+  # last_two = int(str(inputs[-2]) + str(inputs[-1]))
+  # concat_inputs = inputs[:-2] + [last_two]
+  # print('inputs:', inputs, 'last_two', last_two, 'contact: ',concat_inputs)
+  chars_to_remove = len(str(inputs[-1]))
+  if len(str(value)) > chars_to_remove and str(value)[-chars_to_remove:] == str(inputs[-1]):
+    # print('value', value, "inputs", inputs)
+    value_without_last_char = int(str(int(value))[:-chars_to_remove])
+    return is_solvable(value-inputs[-1], inputs[:-1]) or is_solvable(value/inputs[-1], inputs[:-1]) or is_solvable(value_without_last_char, inputs[:-1])
+  else: 
+    # print('value', value,'len(str(value))', len(str(value)), 'inputs', inputs, 'chars_to_remove', chars_to_remove)
+    return is_solvable(value-inputs[-1], inputs[:-1]) or is_solvable(value/inputs[-1], inputs[:-1]) 
+
 
 total = 0
 # max_length = 0
 for eq in eqs:
   # total += solve(eq)
+  print('---------')
   value = int(eq.split(': ')[0])
+  print(value)
+  print('---------')
+
   inputs = [int(i) for i in eq.split(': ')[1].split(' ')]
   if is_solvable(value, inputs):
+    print('solvable', value)
     total += value
+  else:
+    print('NOT solvableable', value)
 print(total)
 
+# 3782298305575 is not right
+# 3782298305575 is not right
