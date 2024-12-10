bf5664e3ffc451a47849bbe5d78eb87bde833b85
Brittney Johnson <brittneyejohnson@gmail.com>
2024-12-10 10:21:09 -0500

    d10p2

--- 2024/day10.py
+++ 2024/day10.py
@@ -28,32 +28,29 @@ def trailhead_str(trailhead):
   return f"{trailhead[0]},{trailhead[1]}"  
 
 def score_trailhead(trailhead, map):
-  total_score = set([])
+  total_score = 0
   trailhead_score = map[trailhead[0]][trailhead[1]]
-  # if trailhead_str(trailhead) in location_scores:
-  #   return location_scores[trailhead_str(trailhead)]
+  if trailhead_str(trailhead) in location_scores:
+    return location_scores[trailhead_str(trailhead)]
   if trailhead_score == 9:
-    # print('found',trailhead_str(trailhead), 'returning',set([trailhead_str(trailhead)]))
-    total_score.add(trailhead_str(trailhead))
-    return total_score
-
+    return 1
   up_trailhead = [trailhead[0]-1,trailhead[1]]
   if is_valid(up_trailhead) and map[up_trailhead[0]][up_trailhead[1]] == trailhead_score + 1:
-   total_score = total_score.union(score_trailhead(up_trailhead, map))
+   total_score += score_trailhead(up_trailhead, map)
 
   down_trailhead = [trailhead[0]+1,trailhead[1]]
   if is_valid(down_trailhead) and map[down_trailhead[0]][down_trailhead[1]] == trailhead_score + 1:
-   total_score = total_score.union(score_trailhead(down_trailhead, map))
+   total_score += score_trailhead(down_trailhead, map)
 
   left_trailhead = [trailhead[0],trailhead[1]-1]
   if is_valid(left_trailhead) and map[left_trailhead[0]][left_trailhead[1]] == trailhead_score + 1:
-   total_score = total_score.union(score_trailhead(left_trailhead, map))
+   total_score += score_trailhead(left_trailhead, map)
 
   right_trailhead = [trailhead[0],trailhead[1]+1]
   if is_valid(right_trailhead) and map[right_trailhead[0]][right_trailhead[1]] == trailhead_score + 1:
-   total_score = total_score.union(score_trailhead(right_trailhead, map))
+   total_score += score_trailhead(right_trailhead, map)
 
-  # location_scores[trailhead_str(trailhead)] = total_score
+  location_scores[trailhead_str(trailhead)] = total_score
   return(total_score)
 
 
@@ -61,9 +58,8 @@ def score_trailhead(trailhead, map):
 
 map = helpers.parse_int_map_from_input(input)
 trailheads = find_trailheads(map)
-print(trailheads)
 total = 0
 for trailhead in trailheads:
-  total += len(score_trailhead(trailhead, map))
-  # print(trailhead, len(score_trailhead(trailhead, map)))
+  total += score_trailhead(trailhead, map)
+  # print(trailhead, score_trailhead(trailhead, map))
 print(total)
