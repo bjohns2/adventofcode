f435b2a4977b955fa02764036a5afda3d1050d3f
Brittney Johnson <brittneyejohnson@gmail.com>
2024-12-15 09:54:38 -0500

    d14p2

--- 2024/day14.py
+++ 2024/day14.py
@@ -1,13 +1,16 @@
 import helpers
+import math
 my_input = open("./2024/my_input.txt", "r").read()
 test_input = open("./2024/test_input.txt", "r").read()
-input = my_input
 
 # HEIGHT = 7 
 # WIDTH = 11
+# input = test_input
 
 HEIGHT = 103
 WIDTH = 101
+input = my_input
+
 SECONDS = 100
 
 # p=9,5 v=-3,-3
@@ -53,12 +56,39 @@ def calculate_quadrants(locs):
   return quads
 
 robots = parse_input(input)
-locs = []
-for robot in robots:
-  loc = get_location_on_second(robot, SECONDS)
-  locs.append(loc)
+START_RANGE = 0
+END_RANGE = 100000
+RANGE_SIZE = END_RANGE - START_RANGE
+
+BOTTOM_ROW = math.floor(HEIGHT * .6)
+MIDDLE = WIDTH//2
+print(BOTTOM_ROW,MIDDLE)
+
+def any_robots_touch_enough(locs):
+  for loc in locs:
+    if (loc[0],loc[1]+1) in locs and  (loc[0],loc[1]-1) in locs and   (loc[0]-1,loc[1]+1) in locs and   (loc[0]-1,loc[1]-1) in locs and (loc[0]+1,loc[1]+1) in locs and  (loc[0]+1,loc[1]-1) in locs:
+      return True
+  return False
+
+for i in range(START_RANGE,END_RANGE):
+  locs = []
+  for robot in robots:
+    loc = get_location_on_second(robot, i)
+    locs.append(loc)
+  if any_robots_touch_enough(locs):
+    print(f"====={i}=====")
+    print_locs(locs)
+    print(f"====={i}=====")
+  # quads = calculate_quadrants(locs)
+  # quads[0] == quads[1] and quads[2] == quads[3] and quads[0] > quads[2]: #
+  # if (MIDDLE-1, BOTTOM_ROW) in locs and (MIDDLE, BOTTOM_ROW) in locs and (MIDDLE+1,BOTTOM_ROW) in locs: # and (MIDDLE,BOTTOM_ROW+1) in locs:
+  #   print_locs(locs)
+  #   print("==========")
+  if i % (RANGE_SIZE//10) == 0:
+    print(f"============================== {i//(RANGE_SIZE//100)}% =======================================")
   
-print_locs(locs)
-quads = calculate_quadrants(locs)
-print(quads)
-print('total',quads[0]*quads[1]*quads[2]*quads[3])
+print('done')
+# print_locs(locs)
+# quads = calculate_quadrants(locs)
+# print(quads)
+# print('total',quads[0]*quads[1]*quads[2]*quads[3])
