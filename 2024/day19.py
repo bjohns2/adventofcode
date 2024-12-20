9d94b8032cd6adadfd40674dc040b10a246a77bd
Brittney Johnson <brittneyejohnson@gmail.com>
2024-12-19 21:42:52 -0500

    d19p2

--- 2024/day19.py
+++ 2024/day19.py
@@ -5,27 +5,27 @@ input = my_input
 available_towels = [i.strip() for i in input.split("\n")[0].split(',')]
 designs = input.split("\n")[2:]
 
-cache = {'': True}
+cache = {'': 1}
 
 print('available_towels',','.join(available_towels))
 def check_design(design,available_towels):
   if design in cache:
     return cache[design]
+  total_per_towels = 0
   for towel in available_towels:
     towel_size = len(towel)
     if design[:towel_size] == towel:
       design_check = check_design(design[towel_size:],available_towels)
-      if design_check:
-        cache[design] = True
-        return True
-  cache[design] = False
-  return False
+      total_per_towels += design_check
+      # if design_check:
+      # cache[design] = design_check
+      # return design_check
+  cache[design] = total_per_towels
+  return total_per_towels
 
 total = 0
 for design in designs:
-  valid = check_design(design,available_towels)
-  if valid:
-    total +=1
-  else:
-    print(design,valid)
+  design_check = check_design(design,available_towels)
+  total += design_check
+  print(design,design_check)
 print(total)
