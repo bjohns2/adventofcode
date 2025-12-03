c3e853d6cae929190842be1739789dc10c83a55b
Brittney Johnson <brittneyejohnson@gmail.com>
2025-12-03 09:32:33 -0500

    2025 d3 p2

--- 2025/day3.py
+++ 2025/day3.py
@@ -7,34 +7,29 @@ test_input = test_input_txt.read()
 
 input = my_input
 
-def find_biggest(bank):
-  first_char_index = -1
-  second_char_index = -1
-
+def find_biggest(bank, length):
+  # print("finding biggest of", bank,"length",length)
+  char_index = -1
+  bank_to_check = bank if length == 1 else bank[:(-1*(length-1))]
   for i in range(0,10):
-    if str(i) in bank[:-1]:
-      first_char_index = bank.index(str(i))
-
-
-  for i in range(0,10):
-    # print(i, str(i) in bank[first_char_index+1:], second_char_index)
-    if str(i) in bank[first_char_index+1:]:
-      second_char_index = (bank[first_char_index+1:]).index(str(i)) + first_char_index + 1
-
+    # print("checking in ",bank_to_check)
+    if str(i) in bank_to_check:
+      char_index = bank.index(str(i))
   
-  biggest = bank[first_char_index] + bank[second_char_index]
-  return int(biggest)
+  if length == 1:
+    return bank[char_index]
+  else:
+    return bank[char_index]+find_biggest(bank[char_index+1:], length-1)
 
 
 
 def solution():
   total = 0
   banks = input.split("\n")
-  biggest = find_biggest(banks[0])
   for bank in banks:
-    print(bank)
-    biggest = find_biggest(bank)
-    # print(bank, biggest)
+    # print(bank)
+    biggest = int(find_biggest(bank,12))
+    print(bank, biggest)
     total += biggest
   return total
 
