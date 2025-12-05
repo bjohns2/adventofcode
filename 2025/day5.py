a996a56173ca2ad7b1a6d7fbd38150e41e66bd87
Brittney Johnson <brittneyejohnson@gmail.com>
2025-12-05 08:23:32 -0500

    2025 d5 p2

--- 2025/day5.py
+++ 2025/day5.py
@@ -10,14 +10,27 @@ def solution():
   inputs = input.split("\n\n")
   string_ranges = inputs[0].split("\n")
   ranges = [transform_range(r) for r in string_ranges]
-  ingredients = inputs[1].split("\n")
+  string_ingredients = inputs[1].split("\n")
+  ingredients = [int(i) for i in string_ingredients]
+
+  ranges.sort( key=lambda x: x[0])
 
   count = 0
-  for ingredient in ingredients:
-    for range in ranges:
-      if int(ingredient) in range:
-        count +=1 
-        break
+  start = ranges[0][0]
+  end = ranges[0][-1]+1
+  for r in ranges:
+    # print("looking at",r,', current run is',range(start,end))
+    # if you start before the last one ends, continue it
+    if r[0] <= end:
+      end = max(r[-1]+1,end)
+    # else, you start after the last one ends; need to count up the last one and start a new one
+    else:
+      # print("found range from",start,'to',end,', adding', end-start)
+      count += (end-start)
+      start = r[0]
+      end = r[-1]+1
+  # print("adding final range from",start,'to',end,', adding', end-start)
+  count += (end-start)
   return count
 
 
